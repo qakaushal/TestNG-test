cb19f3e38a4598b3835b116f1c28716adb426ddc
qakaushal <knandvidkar1226@gmail.com>
2024-05-09 11:37:09 +0530

    first commit

---
+++ src/testNGTutorial/TestDemo.java
@@ -0,0 +1,16 @@
+package testNGTutorial;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+	@Test
+	public void print() {
+		System.out.println("ABC");
+	}
+	
+	@Test
+	public void hello() {
+		System.out.println("XYZ");
+	}
+
+}
